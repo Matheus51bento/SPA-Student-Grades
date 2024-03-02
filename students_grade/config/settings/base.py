0a3d53807578c68f9edc20d741b904b0af429562
Matheus Bento <matheus51bento@gmail.com>
2024-03-01 21:01:56 -0300

    database

--- students_grade/config/settings/base.py
+++ students_grade/config/settings/base.py
@@ -52,6 +52,7 @@ DATABASES = {
         default="postgres:///students_grade",
     ),
 }
+
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
